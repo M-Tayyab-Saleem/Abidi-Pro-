8f46fd0edf38f491fd663b78f11dd14d43b3da52
M Tayyab Saleem <tayyabsknowledge985@gmail.com>
2025-05-06 20:28:16 +0500

    Edit Error message

--- middlewares/globalErrorHandler.js
+++ middlewares/globalErrorHandler.js
@@ -1,31 +1,34 @@
 const { ExpressError, BadRequestError } = require("../utils/ExpressError");
 
 const globalErrorHandler = (err, req, res, next) => {
-  
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors)
+      .map(val => val.message)
+      .join(", ");
+    err = new BadRequestError(message);
+  }
 
-    if (err.name === "ValidationError") {
-      const message = Object.values(err.errors)
-        .map(val => val.message)
-        .join(", ");
-      err = new BadRequestError(message);
-    }
-  
-  
-  
-    if (err.name === "CastError") {
-      err = new BadRequestError(`Invalid ${err.path}: ${err.value}`);
-    }
-  
-    const statusCode = err.status || 500;
-    const message = err.message || "Something went wrong";
-    
-    console.error("Error:", err);
-    res.status(statusCode).json({
-      success: false,
-      status: statusCode,
-      message,
-      stack: process.env.NODE_ENV === "development" ? err.stack : undefined, 
-    });
-  };
+  if (err.name === "CastError") {
+    err = new BadRequestError(`Invalid ${err.path}: ${err.value}`);
+  }
 
-  module.exports = globalErrorHandler;
+  if (err.code === 11000) {
+    const field = Object.keys(err.keyValue)[0]; // e.g. "contact"
+    const value = err.keyValue[field];          // e.g. "+923090769754"
+    const friendlyMessage = `This ${field} is already registered: ${value}`;
+    err = new BadRequestError(friendlyMessage);
+  }
+
+  const statusCode = err.status || 500;
+  const message = err.message || "Something went wrong";
+
+  console.error("Error:", err);
+  res.status(statusCode).json({
+    success: false,
+    status: statusCode,
+    message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+  });
+};
+
+module.exports = globalErrorHandler;
