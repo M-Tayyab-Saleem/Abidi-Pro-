0ec9e6cec463e1bfedf1db9a2560e0112f143343
M Tayyab Saleem <tayyabsknowledge985@gmail.com>
2025-05-19 16:25:11 +0500

    Initial commit

--- conn/conn.js
+++ conn/conn.js
@@ -1,23 +1,13 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv")
 
-dotenv.config();
+dotenv.config()
 
 const conn = async () => {
   try {
-    console.log("Connecting to:", process.env.MONGODB_URI);
-
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("Database Connected Successfully!!");
-
-    mongoose.connection.on('error', err => {
-      console.error('Mongoose connection error:', err);
-    });
-
+    await mongoose.connect(
+     process.env.MONGODB_URI)
+    console.log("DataBase Connected Successfully!!");
   } catch (error) {
     console.error("Database connection failed:", error);
   }
