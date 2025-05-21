bf2c8d9720c28a58c05a19b5378a916d80d0d003
M-Tayyab-Saleem <144640984+M-Tayyab-Saleem@users.noreply.github.com>
2025-05-21 16:31:10 +0500

    Update refreshTokenMiddleware.js
    lax cookies

--- middlewares/refreshTokenMiddleware.js
+++ middlewares/refreshTokenMiddleware.js
@@ -75,14 +75,14 @@ const refreshTokenMiddleware = catchAsync(async (req, res, next) => {
     res.cookie("token", newToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "None",
+      sameSite: "Lax",
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", newRefreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "None",
+      sameSite: "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
