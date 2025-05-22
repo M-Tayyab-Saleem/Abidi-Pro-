31364e54f8709e25ff80c42c0f081c64f597e6d3
M-Tayyab-Saleem <144640984+M-Tayyab-Saleem@users.noreply.github.com>
2025-05-22 18:03:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
 require('dotenv').config();
 
 const express = require("express");
-const dotenv = require('dotenv');
 const cors = require("cors");
 require("./conn/conn");
-const app = express();
-const { ExpressError } = require("./utils/ExpressError");
 const cookieParser = require("cookie-parser");
 const refreshTokenMiddleware = require('./middlewares/refreshTokenMiddleware');
-
-
-require("./utils/cronScheduler");  
-const PORT = process.env.PORT || 3000;
-
-
-const allRoutes = require("./routes/allRoutes");
-const webRoutes = require("./routes/webRoutesMount");
 const globalErrorHandler = require('./middlewares/globalErrorHandler');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://b668-2407-aa80-15-6e08-115f-7abb-9729-15d3.ngrok-free.app', 'http://localhost:3001', 'exp://192.168.18.10:8081'],
- credentials: true, 
+  origin: ['http://localhost:5173', 'http://localhost:3000', "http://localhost:5174","http://localhost:5175"],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, 
+    credentials: true,                
+  })
+);
 
-app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-// API routes
-app.get('/', (req, res) => {
-  res.send("Hello");
-});
-
+// Refresh Token Middleware globally
 app.use(refreshTokenMiddleware);
 
-app.use('/api',allRoutes)
-app.use("/api/web", webRoutes);
+// Routes
+app.use('/api', require('./routes/allRoutes'));
+app.use('/api/web', require('./routes/webRoutesMount'));
 
 app.all("*", (req, res, next) => {
-  err = new ExpressError(404, "Page not Found");
-  next(err);
+  const { ExpressError } = require("./utils/ExpressError");
+  next(new ExpressError(404, "Page not Found"));
 });
 
 app.use(globalErrorHandler);
