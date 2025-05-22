da0e595848b6c2ee81b8f432347fcdadd39f1147
M-Tayyab-Saleem <144640984+M-Tayyab-Saleem@users.noreply.github.com>
2025-05-22 18:02:00 +0500

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,42 +1,34 @@
 const jwt = require('jsonwebtoken');
 const { UnauthorizedError } = require('../utils/ExpressError');
 const { error } = require('../utils/logger');
-const BlacklistedToken = require('../models/BlacklistedTokenSchema'); 
+const BlacklistedToken = require('../models/BlacklistedTokenSchema');
 
 
 const isLoggedIn = async (req, res, next) => {
   try {
-    let token;
+    let token = req.cookies?.token;
 
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      token = req.headers.authorization?.split(' ')[1];
+    if (!token && req.headers.authorization?.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
     }
 
-    if (!token && req.cookies && req.cookies.token) {
-      token = req.cookies.token;
-    }
-
-   
     if (!token) {
       error('No authentication token found');
-      throw new UnauthorizedError('Please login First.');
+      throw new UnauthorizedError('Please login first.');
     }
 
-  
     const blacklistedToken = await BlacklistedToken.findOne({ token });
     if (blacklistedToken) {
       throw new UnauthorizedError('Token is no longer valid. Please login again.');
     }
 
-   
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
     error(`Auth Middleware Error: ${err.message}`);
-    next(err); 
+    next(err);
   }
 };
-
 
 module.exports = { isLoggedIn };
