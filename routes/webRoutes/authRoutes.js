2d2814a9e4b75e6a0ec65b620ead00c5b5be3f62
M-Tayyab-Saleem <144640984+M-Tayyab-Saleem@users.noreply.github.com>
2025-05-22 17:10:21 +0500

    Update authRoutes.js
    check session

--- routes/webRoutes/authRoutes.js
+++ routes/webRoutes/authRoutes.js
@@ -16,6 +16,8 @@ router.post("/verify-otp", catchAsync(authController.verifyOtp));
 router.post("/resend-otp", catchAsync(authController.resendOtp));
 router.post("/logout",  catchAsync(authController.logout));
 router.get("/me", isLoggedIn, catchAsync(authController.getCurrentUser));
+router.get("/check-session", isLoggedIn, catchAsync(authController.getCurrentUser));
+
 
 // Forgot password routes
 router.post("/forgot-password", catchAsync(authController.forgotPassword));
